fd70a2c7325acca143a0618d38967a1cccbb340b
ciyingzuo <huntercyz@outlook.com>
2018-07-18 08:09:14 +0800

    3

--- src/main/java/com/example/WebProject/services/LessonService.java
+++ src/main/java/com/example/WebProject/services/LessonService.java
@@ -1,11 +1,45 @@
 package com.example.WebProject.services;
 
+import com.example.WebProject.model.Course;
+import com.example.WebProject.model.Lesson;
 import com.example.WebProject.repository.LessonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LessonService {
     @Autowired
-    LessonRepository courseRepository;
+    LessonRepository lessonRepository;
+
+//    createLesson
+//    creates a lesson for a module
+//    POST /api/course/{cid}/module/{mid}/lesson
+//1
+//    deleteLesson
+//    deletes a lesson by id
+//    DELETE /api/lesson/{id}
+//1
+//    findAllLessons
+//    retrieves all the lessons
+//    GET /api/lesson
+//1
+//    findLessonById
+//    retrieves a lesson by id
+//    GET /api/lesson/{id}
+//0
+//    findAllLessonssForModule
+//    retrieves all lessons for module
+//    GET /api/course/{cid}/module/{mid}/lesson
+//1
+//    updateLesson
+//    updates a lesson by id
+//    PUT /api/lesson/{id}
+//0
+
+    @PostMapping("/api/lesson")
+    public Lesson createCourse(@RequestBody Lesson lesson) {
+        return lessonRepository.save(lesson);
+    }
 }
--- src/main/java/com/example/WebProject/services/ModuleService.java
+++ src/main/java/com/example/WebProject/services/ModuleService.java
@@ -1,11 +1,44 @@
 package com.example.WebProject.services;
 
+import com.example.WebProject.model.Module;
 import com.example.WebProject.repository.ModuleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ModuleService {
     @Autowired
-    ModuleRepository courseRepository;
+    ModuleRepository moduleRepository;
+//    createModule
+//    creates a module for a course
+//    POST /api/course/{cid}/module
+//1
+//    deleteModule
+//    deletes a module by id
+//    DELETE /api/module/{id}
+//1
+//    findAllModules
+//    retrieves all the modules
+//    GET /api/module
+//1
+//    findModuleById
+//    retrieves a module by id
+//    GET /api/module/{id}
+//0
+//    findAllModulesForCourse
+//    retrieves all modules for course
+//    GET /api/course/{cid}/module
+//1
+//    updateModule
+//    updates a module by id
+//    PUT /api/module/{id}
+//0
+
+
+    @PostMapping("/api/module")
+    public Module createCourse(@RequestBody Module lesson) {
+        return moduleRepository.save(lesson);
+    }
 }
